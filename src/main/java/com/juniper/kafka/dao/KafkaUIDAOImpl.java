29efa8eadc2423707a47bdb785ab074082549266
Razak1985 <Razak1985@users.noreply.github.com>
2019-01-06 19:05:28 +0000

    razak

--- src/main/java/com/juniper/kafka/dao/KafkaUIDAOImpl.java
+++ src/main/java/com/juniper/kafka/dao/KafkaUIDAOImpl.java
@@ -1,12 +1,14 @@
 package com.juniper.kafka.dao;
 
-import com.juniper.kafka.dto.KafkaUIDto;
-
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import com.juniper.kafka.dto.ConsumerKafkaUIDTO;
+import com.juniper.kafka.dto.KafkaTopicUIDTO;
+import com.juniper.kafka.dto.KafkaUIDto;
 
 public class KafkaUIDAOImpl extends JdbcDaoSupport implements KafkaUIDAO{
 
@@ -28,5 +30,24 @@ public class KafkaUIDAOImpl extends JdbcDaoSupport implements KafkaUIDAO{
 		});*/
 		return "";
 	}
+	
+	@Override
+	public String saveUIConsumer(ConsumerKafkaUIDTO kafkaUIDto) {
+		/*String sql = "INSERT INTO juiperkafka " +
+				"(host, port) VALUES (?, ?)" ;
+		getJdbcTemplate().update(sql, new Object[]{
+				kafkaUIDto.getHostName(), kafkaUIDto.getPort()
+		});*/
+		return "";
+	}
 
+	@Override
+	public String saveUITopic(KafkaTopicUIDTO kafkaUIDto) {
+		/*String sql = "INSERT INTO juiperkafka " +
+				"(host, port) VALUES (?, ?)" ;
+		getJdbcTemplate().update(sql, new Object[]{
+				kafkaUIDto.getHostName(), kafkaUIDto.getPort()
+		});*/
+		return "";
+	}
 }
